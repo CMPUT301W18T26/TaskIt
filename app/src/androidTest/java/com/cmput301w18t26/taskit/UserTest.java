c9d2448d606f0ffcc0f543598cbb37fb2e8378e9
ktgordon <ktgordon@ualberta.ca>
2018-02-26 10:18:13 -0700

    modified build.gradle to allow backcompat to sdk 18 (for unit testing we learned in lab). Added blank model class test classes.

---
+++ app/src/androidTest/java/com/cmput301w18t26/taskit/UserTest.java
@@ -0,0 +1,18 @@
+package com.cmput301w18t26.taskit;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by kevingordon on 2018-02-26.
+ */
+
+public class UserTest extends ActivityInstrumentationTestCase2 {
+    public UserTest() {
+        super(UserActivity.class);
+    }
+
+    public void testAdd() {}
+    public void testHas() {}
+    public void testGet() {}
+    public void testDelete() {}
+}
