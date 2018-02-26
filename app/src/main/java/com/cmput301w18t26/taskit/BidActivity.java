4756b25a09b31f3b79bef0917f34b59eb1a2dec8
ktgordon <ktgordon@ualberta.ca>
2018-02-26 09:59:51 -0700

    Blank model classes created

---
+++ app/src/main/java/com/cmput301w18t26/taskit/BidActivity.java
@@ -0,0 +1,17 @@
+package com.cmput301w18t26.taskit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kevingordon on 2018-02-26.
+ */
+
+public class BidActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bid);
+    }
+}
