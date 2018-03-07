097ec99e112466a34426ddecc34471e746e4fef0
Sharon Hains <hains@ualberta.ca>
2018-03-07 05:17:31 -0700

    Updated XMLs

--- app/src/main/java/com/cmput301w18t26/taskit/ListActivity.java
+++ app/src/main/java/com/cmput301w18t26/taskit/ListActivity.java
@@ -3,12 +3,17 @@ package com.cmput301w18t26.taskit;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 /**
  * Created by kevingordon on 2018-02-26.
  */
 
 public class ListActivity extends AppCompatActivity {
+
+    private ListView bidlist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +22,14 @@ public class ListActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String type = intent.getStringExtra(HomeActivity.TYPE);
         setTitle(type);
+
+        bidlist = (ListView) findViewById(R.id.tasklist);
+
+        bidlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, final int i, long l) {
+            }
+        });
+
     }
 }
