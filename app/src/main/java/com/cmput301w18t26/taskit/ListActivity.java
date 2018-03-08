04bd110031a3ae4f7cec4845b3fd0df4e950bdce
Colin D'Amore <cdamore@ualberta.ca>
2018-03-08 03:29:16 -0700

    added task functionality to list activity

--- app/src/main/java/com/cmput301w18t26/taskit/ListActivity.java
+++ app/src/main/java/com/cmput301w18t26/taskit/ListActivity.java
@@ -3,12 +3,17 @@ package com.cmput301w18t26.taskit;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by kevingordon on 2018-02-26.
@@ -16,9 +21,18 @@ import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
 
+    protected static final String TITLE = "title";
+    protected static final String LOCATION = "location";
+    protected static final String OWNER = "owner";
+    protected static final String DESCRIPTION = "description";
+    protected static final String STATUS = "status";
+    protected static final String DATE = "date";
     protected static final String TYPE = "type";
+    protected static final String POSITION = "position";
+
     private ListView listOfTasks;
-    private ArrayList<Task> TaskList;
+    private ArrayList<Task> taskList = new ArrayList<Task>();
+    private Task task1 = new Task();
     private TaskAdapter adapter;
     private TaskItData db;
     TaskList tasks;
@@ -32,7 +46,6 @@ public class ListActivity extends AppCompatActivity {
         listOfTasks = (ListView) findViewById(R.id.listOfTasks);
         db = TaskItData.getInstance();
         db.refresh();
-        TaskList = db.getTasks().getTasks();
         Intent intent = getIntent();
         String type = intent.getStringExtra(HomeActivity.TYPE);
         setTitle(type);
@@ -69,10 +82,57 @@ public class ListActivity extends AppCompatActivity {
     protected void onStart() {
         // TODO Auto-generated method stub
         super.onStart();
-        //loadFromFile();
-        adapter = new TaskAdapter(ListActivity.this, TaskList);
+        taskList = db.getTasks().getTasks();
+        task1.setTitle("Water Flower");
+        task1.setLocation("Earth");
+        task1.setOwner("Kietherson");
+        task1.setDescription("Water a flower.");
+        task1.setStatus("open");
+        Date myDate = new Date(2014, 02, 11);
+        task1.setDate(myDate);
+        taskList.add(task1);
+        Log.w("taskList", taskList.toString());
+        adapter = new TaskAdapter(ListActivity.this, taskList);
         listOfTasks.setAdapter(adapter);
-        //listOfTasks.setOnItemClickListener(new ListClickHandler());
+        listOfTasks.setOnItemClickListener(new ListClickHandler());
+    }
+
+    public class ListClickHandler implements OnItemClickListener{
+
+        /**
+         * Activate DisplayEditSubscription when list item is clicked
+         *
+         * @param adapter current adapter
+         * @param view current view
+         * @param position current position of subscription
+         * @param arg3 excess argument
+         */
+        @Override
+        public void onItemClick(AdapterView<?> adapter, View view, int position, long arg3) {
+            // TODO Auto-generated method stub
+            Task task = (Task)adapter.getItemAtPosition(position);
+            String title = task.getTitle();
+            String location = task.getLocation();
+            String owner = task.getOwner();
+            DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+            String date = df.format(task.getDate());
+            String description = task.getDescription();
+            String status = task.getStatus();
+
+
+            Intent intent = new Intent(ListActivity.this, TaskActivity.class);
+            intent.putExtra(TYPE, "Task");
+            intent.putExtra(TITLE, title);
+            intent.putExtra(LOCATION, location);
+            intent.putExtra(OWNER, owner);
+            intent.putExtra(DATE, date);
+            intent.putExtra(DESCRIPTION, description);
+            intent.putExtra(STATUS, status);
+            intent.putExtra(POSITION, position);
+
+            startActivity(intent);
+
+        }
     }
 
 }
--- app/src/main/java/com/cmput301w18t26/taskit/TaskActivity.java
+++ app/src/main/java/com/cmput301w18t26/taskit/TaskActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by kevingordon on 2018-02-26.
@@ -13,6 +15,13 @@ import android.widget.Button;
 public class TaskActivity extends AppCompatActivity {
 
     protected static final String TYPE = "type";
+
+    private TextView titleText;
+    private TextView dateText;
+    private TextView statusText;
+    private TextView descriptionText;
+    private TextView ownerText;
+    private TextView locationText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +42,27 @@ public class TaskActivity extends AppCompatActivity {
             });
         } else {
             setContentView(R.layout.viewtask);
+            String title = intent.getStringExtra(ListActivity.TITLE);
+            String description = intent.getStringExtra(ListActivity.DESCRIPTION);
+            String status = intent.getStringExtra(ListActivity.STATUS);
+            String owner = intent.getStringExtra(ListActivity.OWNER);
+            String location = intent.getStringExtra(ListActivity.LOCATION);
+            String date = intent.getStringExtra(ListActivity.DATE);
+
+            titleText = (TextView) findViewById(R.id.tasktitle);
+            descriptionText = (TextView) findViewById(R.id.taskdescription);
+            statusText = (TextView) findViewById(R.id.taskstatus);
+            locationText = (TextView) findViewById(R.id.tasklocation);
+            ownerText = (TextView) findViewById(R.id.taskowner);
+            dateText = (TextView) findViewById(R.id.taskdate);
+
+            titleText.setText(title);
+            descriptionText.setText(description);
+            statusText.setText(status);
+            locationText.setText(location);
+            ownerText.setText(owner);
+            dateText.setText(date);
+
         }
         setTitle(type);
     }
