d07de982557680edcaa214ccb3d7bd13e7c691a4
Sharon Hains <hains@alberta.ca>
2018-03-17 17:59:24 -0600

    added bid dialog and bidprompt xml

--- app/src/main/java/com/cmput301w18t26/taskit/BidListActivity.java
+++ app/src/main/java/com/cmput301w18t26/taskit/BidListActivity.java
@@ -1,5 +1,7 @@
 package com.cmput301w18t26.taskit;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -45,39 +47,56 @@ public class BidListActivity extends AppCompatActivity {
         adapter = new BidListAdapter(BidListActivity.this, bidList);
 
         bidlistview.setAdapter(adapter);
-        bidlistview.setOnItemClickListener(new ListClickHandler());
+        //bidlistview.setOnItemClickListener(new ListClickHandler());
 
         bidlistview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
         @Override
         public void onItemClick(AdapterView<?> adapterView, View view, final int i, long l) {
             Bid bid = (Bid) adapter.getItem(i);
+            //Bid bid = (Bid)adapter.getItemAtPosition(i);
+
+            View promptview = getLayoutInflater().inflate(R.layout.bid_prompt,null);
+            AlertDialog.Builder bidprompt = new AlertDialog.Builder(BidListActivity.this);
+            Button acceptBid = (Button) promptview.findViewById(R.id.acceptbid);
+            Button declineBid = (Button) promptview.findViewById(R.id.declinebid);
+
+            bidprompt.setView(promptview);
+            AlertDialog dialog = bidprompt.create();
+            dialog.show();
+            //String UUID = bid.getUUID();
         }
         });
 
 
     }
 
-
-    public class ListClickHandler implements AdapterView.OnItemClickListener {
-
-        /**
-         * Activate DisplayEditSubscription when list item is clicked
-         *
-         * @param adapter current adapter
-         * @param view current view
-         * @param position current position of subscription
-         * @param arg3 excess argument
-         */
-        @Override
-        public void onItemClick(AdapterView<?> adapter, View view, int position, long arg3) {
-            // TODO Auto-generated method stub
-            Bid bid = (Bid)adapter.getItemAtPosition(position);
-            Intent intent = new Intent(BidListActivity.this, BidActivity.class);
-            String UUID = bid.getUUID();
-            intent.putExtra("UUID", UUID);
-
-            startActivity(intent);
-
-        }
-    }
+//
+//    public class ListClickHandler implements AdapterView.OnItemClickListener {
+//
+//        /**
+//         * Activate DisplayEditSubscription when list item is clicked
+//         *
+//         * @param adapter current adapter
+//         * @param view current view
+//         * @param position current position of subscription
+//         * @param arg3 excess argument
+//         */
+//        @Override
+//        public void onItemClick(AdapterView<?> adapter, View view, int position, long arg3) {
+//            // TODO Auto-generated method stub
+//            Bid bid = (Bid)adapter.getItemAtPosition(position);
+//
+//            View promptview = getLayoutInflater().inflate(R.layout.bid_prompt,null);
+//            AlertDialog.Builder bidprompt = new AlertDialog.Builder(BidListActivity.this);
+//            Button acceptBid = (Button) promptview.findViewById(R.id.acceptbid);
+//            Button declineBid = (Button) promptview.findViewById(R.id.declinebid);
+//
+//            bidprompt.setView(promptview);
+//            AlertDialog dialog = bidprompt.create();
+//            dialog.show();
+//            //String UUID = bid.getUUID();
+//
+//
+//        }
+//    }
 }
