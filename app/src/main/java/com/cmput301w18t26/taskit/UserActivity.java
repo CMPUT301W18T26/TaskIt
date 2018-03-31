6da89a6d116a6b96a9dea3cad56d496b1c5eb853
shains <hains@ualberta.ca>
2018-03-30 19:35:57 -0600

    changed login button text

--- app/src/main/java/com/cmput301w18t26/taskit/UserActivity.java
+++ app/src/main/java/com/cmput301w18t26/taskit/UserActivity.java
@@ -3,6 +3,7 @@ package com.cmput301w18t26.taskit;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,7 +38,8 @@ public class UserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         String type = intent.getStringExtra(HomeActivity.TYPE);
-        setTitle(type);
+        //setTitle(type);
+        setTitle(Html.fromHtml("<font color=#ffffff>" + type + "</font>"));
         db = TaskItData.getInstance();
 
         /**
